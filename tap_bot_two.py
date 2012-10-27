1e4f8399fbc31c381f1cf5c6f0277b69febaf05e
Thine-Antique-Pen <tomwikipedia@gmail.com>
2012-10-27 12:31:06 +0200

    Update tap_bot_two.py

--- tap_bot_two.py
+++ tap_bot_two.py
@@ -22,94 +22,37 @@ IN THE SOFTWARE.
 import sys
 import pywikibot
 
-
 site = pywikibot.Site()
-
-
 REDIRECT = '#REDIRECT [[%s]]'
-
-
-
 def create(year):
-
     target = str(year)+' in Mandatory Palestine'
-
     pg1 = pywikibot.Page(site, str(year)+' of the Mandatory Palestinian territories')
-
     pg2 = pywikibot.Page(site, str(year)+' in the Mandatory Palestinian territories')
-
     pg3 = pywikibot.Page(site, str(year)+' in the Mandatory Palestinian National Authority')
-
     pg4 = pywikibot.Page(site, str(year)+' of the Mandatory Palestinian National Authority')
-
     pg5 = pywikibot.Page(site, str(year)+' of Mandatory Palestine')
-
-	
     for page in [pg1, pg2, pg3, pg4, pg5]:
-
-
         if page.exists():
-
-
             continue
-
-
         print 'Creating %s' % page.title(asLink=True)
-
-
         page.put(REDIRECT % target, 'Bot: Creating redirect to [[%s]]' % target)
-
-
         talk = page.toggleTalkPage()
-
-
         if not talk.exists():
-
-
             print 'Creating %s' % talk.title(asLink=True)
-
-
             if 'Palestine' in page.title():
-
-
                 tag = '{{WikiProject Palestine|class=Redirect}}'
-
-
                 summary = 'Bot: tagging article talk page with [[Wikipedia:WikiProject Palestine]]'
-
-
             else:
-
-
                 tag = '{{WikiProject Palestine|class=Redirect}}'
-
-
                 summary = 'Bot: tagging article talk page with [[Wikipedia:WikiProject Palestine]]'
-
-
             talk.put(tag, summary)
-
-
-
 def main():
-
-
     year = 1920
-
-
     while year < 1948:
-
-
         create(year)
-
-
         year += 1
 
 
 if __name__ == "__main__":
-
-
     pywikibot.handleArgs()
-
-
     main()
